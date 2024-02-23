79b4fe6360d02b873b1c0062fc0ac470a08da32b
Hossein <hossienbehzadi1381@gmail.com>
2024-02-23 19:01:51 +0330

    add apple picture and its scale

--- apple.py
+++ apple.py
@@ -1,7 +1,8 @@
 import pygame
 from random import choice
-
 from settings import *
+from math import sin
+
 
 
 class Apple:
@@ -11,14 +12,24 @@ class Apple:
         self.snake = snake
         self.set_position()
 
+        self.surface = pygame.image.load(join('.', 'pictures', 'apple.png')).convert_alpha()
+
+
 
     def draw_apple(self):
-        rect = pygame.Rect(self.pos.x * CELL_SIZE, self.pos.y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-        pygame.draw.rect(self.display_surface, 'blue', rect)
+        # rect = pygame.Rect(self.pos.x * CELL_SIZE, self.pos.y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+        # # pygame.draw.rect(self.display_surface, 'blue', rect)
+        # self.display_surface.blit(self.surface,rect)
+        scale = 1 + sin(pygame.time.get_ticks() / 500) / 3
+        self.scaled_surface = pygame.transform.smoothscale_by(self.surface, scale)
+        self.scaled_rect = self.scaled_surface.get_rect(center = (self.pos.x * CELL_SIZE + CELL_SIZE / 2, self.pos.y * CELL_SIZE + CELL_SIZE / 2))
+
+        self.display_surface.blit(self.scaled_surface, self.scaled_rect)
 
 
     def set_position(self):
         available_positions = [pygame.Vector2(x, y) for x in range(COLS) for y in range(ROWS) if pygame.Vector2(x, y) not in self.snake.body]
         self.pos = choice(available_positions)
+
 
 
